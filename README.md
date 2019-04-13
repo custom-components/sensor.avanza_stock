d97f9a01eb6daa4aea70c6371ba21f2b3935664f
Claes Hallstrom <hallstrom.claes@gmail.com>
2019-04-13 08:18:01 +0200

    Ignore dividends from the past

--- README.md
+++ README.md
@@ -149,6 +149,7 @@ sensor:
 Note: This automation could be further improved by looping over all sensors and checking if their entity_id starts with `sensor.avanza_stock_` and then extract the information.
 
 ## Changelog
+* 0.0.6 - Make sure dividend payment date has not passed
 * 0.0.5 - Add dividends information
 * 0.0.4 - Add companny information (description, marketCapital, sector, totalNumberOfShares)
 * 0.0.3 - Add key ratios (directYield, priceEarningsRatio, volatility)
--- tests/test_avanza_stock.py
+++ tests/test_avanza_stock.py
@@ -1,6 +1,7 @@
 """Avanza Sensor Tests."""
 
 
+import datetime
 import pytest
 from custom_components.avanza_stock.sensor import (
     AvanzaStockSensor, MONITORED_CONDITIONS)
@@ -98,3 +99,53 @@ def test_dividends():
     assert 'dividend0_amountPerShare' in sensor.device_state_attributes
     assert 'dividend0_exDate' in sensor.device_state_attributes
     assert 'dividend0_paymentDate' in sensor.device_state_attributes
+
+
+def test_dividend_future():
+    """Test that dividend happens in the future."""
+    stock = 5266
+    name = 'Avanza Stock {0}'.format(stock)
+    monitored_conditions = ['dividends']
+
+    sensor = AvanzaStockSensor(stock, name, monitored_conditions)
+
+    sensor.update()
+
+    assert 'dividend0_paymentDate' in sensor.device_state_attributes
+
+    payment_date = datetime.datetime.strptime(
+        sensor.device_state_attributes['dividend0_paymentDate'], '%Y-%m-%d')
+    assert payment_date >= datetime.datetime.now()
+
+
+def test_dividend_invalid_keys():
+    """Test stock with invalid dividend keys."""
+    stock = 362015
+    name = 'Avanza Stock {0}'.format(stock)
+    monitored_conditions = ['dividends']
+
+    sensor = AvanzaStockSensor(stock, name, monitored_conditions)
+
+    sensor.update()
+
+    assert 'dividend0_amountPerShare' in sensor.device_state_attributes
+    assert 'dividend0_exDate' in sensor.device_state_attributes
+    assert 'dividend0_paymentDate' in sensor.device_state_attributes
+
+
+def test_dividend_multiple():
+    """Test stock with multiple dividends."""
+    stock = 5234
+    name = 'Avanza Stock {0}'.format(stock)
+    monitored_conditions = ['dividends']
+
+    sensor = AvanzaStockSensor(stock, name, monitored_conditions)
+
+    sensor.update()
+
+    assert 'dividend0_amountPerShare' in sensor.device_state_attributes
+    assert 'dividend0_exDate' in sensor.device_state_attributes
+    assert 'dividend0_paymentDate' in sensor.device_state_attributes
+    assert 'dividend1_amountPerShare' in sensor.device_state_attributes
+    assert 'dividend1_exDate' in sensor.device_state_attributes
+    assert 'dividend1_paymentDate' in sensor.device_state_attributes
