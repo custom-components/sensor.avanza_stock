b3e85d32a335689f33db7542cf8ceeaec40ed20d
Claes Hallström <hallstrom.claes@gmail.com>
2022-01-04 15:36:20 +0100

    Rename deprecated device_state_attributes

--- custom_components/avanza_stock/sensor.py
+++ custom_components/avanza_stock/sensor.py
@@ -188,7 +188,7 @@ class AvanzaStockSensor(Entity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the sensor."""
         return self._state_attributes
 
